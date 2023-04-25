4599e61184a52a78f0a0636ad49ec7c07117370d
Keez <54402072+kbtyne@users.noreply.github.com>
2023-04-25 05:15:42 +0100

    Update voice.js
    The latest working version.

--- voice/voice.js
+++ voice/voice.js
@@ -3,20 +3,31 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import VOICECALL_OBJECT from '@salesforce/schema/VoiceCall';
 import CALLTYPE_FIELD from '@salesforce/schema/VoiceCall.CallType';
 
-const CALL_TYPES={
-    "Call Back":"callback",
-    "Transfer":"transfer",
-    "Inbound":"inbound",
-    "Outbound":"outbound"
-    };
 
 export default class voice extends LightningElement {
     @api recordId;
     voicecallObject = VOICECALL_OBJECT;
-    @wire(getRecord, {  recordId: '$recordId', fields: [CALLTYPE_FIELD]
-}) record;
+    @wire(getRecord, {  
+        recordId: '$recordId', 
+        fields: [CALLTYPE_FIELD],}
+    ) record;
 
     get calltype() {
-        return this.record.data ? getFieldValue(this.record.data, CALLTYPE_FIELD) : ' ';
+          return this.record.data ? getFieldValue(this.record.data, CALLTYPE_FIELD) : ' '
+          ;
     }
+
+    get styleClass(){
+        let styleClass = this.calltype;
+            if (this.calltype == "Inbound") {
+                styleClass='inbound' ;    
+            } else if (this.calltype== "Outbound") {
+                styleClass='outbound';
+            } else if (this.calltype=="Transfer") {
+                styleClass='transfer';
+            } else {
+                styleClass='callback';
+            } 
+        return styleClass;
+     }
 }
